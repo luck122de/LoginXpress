6864567c00d287c3c8772e235528a23966c34c76
AbhirajSingh00 <84569354+AbhirajSingh00@users.noreply.github.com>
2023-07-30 10:13:08 +0530

    just for checking

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ app.get("/",(req,res)=>{
     res.send("u r on home page");
 })
 
-
+app.get("/about",(req,res)=>{
+    res.send("hi");
+})
 
 app.listen(3000,()=>{
     console.log("server get connected");
