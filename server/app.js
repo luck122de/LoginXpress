759faabe83de968337da20c4f4b91124ef946f59
Ankit Rai <railucky390@gmail.com>
2023-07-30 22:42:24 +0530

    added gitignore

--- server/app.js
+++ server/app.js
@@ -1,5 +1,17 @@
 const express=require("express");
 const app=express();
+const mongoose=require("mongoose");
+
+const DB="mongodb+srv://raiankit390:Shreelu323@cluster0.koggrw6.mongodb.net/mernStackretryWritestrue&wmajority"
+
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("connection is successful");
+}).catch((e)=>{
+    console.log(`not connected ${e}`);
+})
 
 app.get("/",(req,res)=>{
     res.send("u r on home page");
