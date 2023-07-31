3bc9f0f9e5e812ef06d534910848876fa4f4e1b3
Ankit Rai <railucky390@gmail.com>
2023-07-31 23:39:52 +0530

    creating data & saving in database

--- server/app.js
+++ server/app.js
@@ -5,11 +5,21 @@ const app=express();
 
 dotenv.config({path: './config.env' });
 
-require("./db/conn");
 
-// const User=require("./module/userSchema");
+
+//require middleware for json
+app.use(express.json());
+
+//link router file for easy rout
+app.use(require('./router/auth'));
+
 
 const PORT=process.env.PORT;
+
+const middleware=(req,res,next)=>{
+   console.log("hello my middleware");
+   next();
+}
 
 
 app.get("/",(req,res)=>{
