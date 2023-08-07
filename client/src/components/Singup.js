bbde659a832d18f61ae543420fa646c56a470a7d
Ankit Rai <railucky390@gmail.com>
2023-08-07 08:11:18 +0530

    Template of signUP page and also added hook state

--- client/src/components/Singup.js
+++ client/src/components/Singup.js
@@ -1,70 +1,118 @@
-import React from 'react'
+import React, {useState}  from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+import signpic from "../images/signup.svg";
 
- const Singup = () => {
-  return (
-     <>
-     <section className='signup'>
-        <div className='container mt-5'>
-            <div className='singup-content'>
-                <div className='signup-form'>
-                  <h2 className='form-title'>sign up</h2>
-             <form className='register-form' id='register-form'>
+const Signup = () => {
+const [user,setUser]=useState({
+    name:"",email:"",phone:"",work:"",password:"",cpassword:""
+});
+let name,value;
+const handleInputs=(e)=>{
+     name=e.target.name;
+     value=e.target.value;
 
-      <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">
-    <i class="zmdi zmdi-name"></i>Name</label>
-    <input type="text" className="form-control" id="exampleInputEmail1" autoComplete='off'
-      placeholder='Enter your Name' 
-    />
- 
-  </div>
-  <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">
-    <i class="zmdi zmdi-email"></i>Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" autoComplete='off'
-      placeholder='Enter your email Id' 
-    />
- 
-  </div>
-  <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">
-    <i class="zmdi zmdi-phone-in-talk"></i>Phone Number</label>
-    <input type="number" className="form-control" id="exampleInputEmail1" autoComplete='off'
-      placeholder='Enter your Phone number' 
-    />
- 
-  </div>
-  <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">
-    <i class="zmdi zmdi-slideshow"></i>Work</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" autoComplete='off'
-      placeholder='Enter your Work' 
-    />
- 
-  </div>
-  <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">
-    <i class="zmdi zmdi-lock"></i>Password</label>
-    <input type="password" className="form-control" id="exampleInputEmail1" autoComplete='off'
-      placeholder='Enter your password' 
-    />
-  </div>
-
-  <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">
-    <i class="zmdi zmdi-lock"></i>Confirm Password</label>
-    <input type="password" className="form-control" id="exampleInputEmail1" autoComplete='off'
-      placeholder='Enter your password' 
-    />
-  </div>
-  
-  <button type="submit" class="btn btn-primary">Register</button>
-                  </form>
-                </div>
-            </div>
-        </div>
-     </section>
-     </>
-  )
+     setUser({user,[name]:[value]});
 }
-export default Singup;
+
+
+
+    return (
+        <>
+            <section className="signup">
+                <div className="container mt-5">
+                    <div className="signup-content">
+                        <div className="signup-form">
+                            <h2 className="form-title">Sign up</h2>
+                            <form method="POST" className="register-form" id="register-form">
+                                
+                                <div className="form-group">
+                                    <label htmlFor="name">
+                                        <i className="zmdi zmdi-account material-icons-name"></i>
+                                    </label>
+                                    <input type="text" name="name" id="name" autocomplete="off"
+                                        value={user.name}
+                                        onChange={handleInputs}
+                                        placeholder="Your Name"
+                                    />
+                                </div>
+
+                                 <div className="form-group">
+                                    <label htmlFor="email">
+                                        <i className="zmdi zmdi-email material-icons-name"></i>
+                                    </label>
+                                    <input type="email" name="email" id="email" autoComplete="off"
+                                        value={user.email}
+                                        onChange={handleInputs}
+                                        placeholder="Your Email"
+                                    />
+                                </div>
+
+                                 <div className="form-group">
+                                    <label htmlFor="phone">
+                                        <i className="zmdi zmdi-phone-in-talk material-icons-name"></i>
+                                    </label>
+                                    <input type="number" name="phone" id="phone" autoComplete="off"
+                                        value={user.phone}
+                                        onChange={handleInputs}
+                                        placeholder="Your Phone"
+                                    />
+                                </div>
+
+                                 <div className="form-group">
+                                    <label htmlFor="work">
+                                        <i className="zmdi zmdi-slideshow material-icons-name"></i>
+                                    </label>
+                                    <input type="text" name="work" id="work" autoComplete="off"
+                                        value={user.work}
+                                        onChange={handleInputs}
+                                        placeholder="Your Profession"
+                                    />
+                                </div>
+
+                                 <div className="form-group">
+                                    <label htmlFor="password">
+                                        <i className="zmdi zmdi-lock material-icons-name"></i>
+                                    </label>
+                                    <input type="password" name="password" id="password" autoComplete="off"
+                                        value={user.password}
+                                        onChange={handleInputs}
+                                        placeholder="Your Password"
+                                    />
+                                </div>
+
+                                <div className="form-group">
+                                    <label htmlFor="cpassword">
+                                        <i className="zmdi zmdi-lock material-icons-name"></i>
+                                    </label>
+                                    <input type="password" name="cpassword" id="cpassword" autoComplete="off"
+                                        value={user.cpassword}
+                                        onChange={handleInputs}
+                                        placeholder="Confirm Your Password"
+                                    />
+                                </div>
+                               
+                                <div className="form-group form-button">
+                                    <input type="submit" name="signup" id="signup" className="form-submit"
+                                        // value="register" onClick={PostData}
+                                     
+                                    />
+                                </div>
+
+                            </form>
+                        </div>
+                        
+                            <div className="signup-image">
+                                <figure>
+                                    <img src={signpic} alt="registration pic" />
+                                </figure>
+                                <NavLink to="/login" className="signup-image-link">I am already register</NavLink>
+                            </div>
+                       
+                    </div>
+                </div>
+           </section>
+        </>
+    )
+}
+
+export default Signup
